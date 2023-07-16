f9db972e2aebac6bf2e3a0ebadd0dd66763a10fc
milad <78633834+Milad-heydarloo@users.noreply.github.com>
2023-07-16 11:31:29 -0700

    TextStyle & Text

--- lib/blog_page.dart
+++ lib/blog_page.dart
@@ -32,18 +32,50 @@ class run_app_class extends StatelessWidget {
     return Scaffold(
       appBar: _app_bar(),
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(8),
-          child: Center(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image(
-                image: AssetImage(
-                  'images/c.png',
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(8),
+              child: Center(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image(
+                    image: AssetImage(
+                      'images/c.png',
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
+            Text(
+              '14 آبان Cosmus سیگنال خرید',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(' سیگنال 12000 فروش',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold)),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(' سیگنال 12000 خرید',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.green,
+                        fontWeight: FontWeight.bold)),
+              ],
+            )
+          ],
         ),
       ),
     );
