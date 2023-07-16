42f6cb957658c2b8e4a3172edd8a8910c2b4285a
milad <78633834+Milad-heydarloo@users.noreply.github.com>
2023-07-16 10:00:46 -0700

    chang color back btn appbar

--- lib/blog_page.dart
+++ lib/blog_page.dart
@@ -13,31 +13,29 @@ class blog_pages extends StatelessWidget {
 
 class run_app_class extends StatelessWidget {
   const run_app_class({super.key});
+  PreferredSizeWidget _app_bar() {
+    return AppBar(
+      backgroundColor: Colors.black,
+      title: Row(children: [
+        Text('VIP ', style: TextStyle(color: Colors.amber)),
+        Text('اخبار سیگنال بورسی')
+      ]),
+      centerTitle: true,
+      iconTheme: IconThemeData(color: Colors.amber),
+    );
+  }
 
   @override
   Widget build(
     BuildContext context,
   ) {
     return Scaffold(
+      appBar: _app_bar(),
       body: SafeArea(
         child: Center(
           child: Column(children: [
             Rating(),
             LikeButton(),
-            TextButton(
-              onPressed: () {
-                Navigator.of(
-                  context,
-                ).push(MaterialPageRoute(
-                  builder: (BuildContext context) {
-                    return run_app_two();
-                  },
-                ));
-              },
-              child: Text(
-                'next',
-              ),
-            ),
           ]),
         ),
       ),
